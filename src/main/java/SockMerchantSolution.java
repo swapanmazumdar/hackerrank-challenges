c024796df33f3a8bc18658675a1d1a0a2d03a25f
Swapan Mazumdar <swapan.mazumdar@gmail.com>
2020-04-03 23:51:55 +0100

    initial commit

---
+++ src/main/java/SockMerchantSolution.java
@@ -0,0 +1,50 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class SockMerchantSolution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] arr) {
+        int pairs = 0;
+        Set<Integer> colors = new HashSet<>();
+
+        for (int item : arr) {
+            if (!colors.contains(item)) {
+                colors.add(item);
+            } else {
+                pairs++;
+                colors.remove(item);
+            }
+        }
+        return pairs;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(arrItems[i]);
+        }
+
+        int result = sockMerchant(n, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}

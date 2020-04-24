3811258b78034ebc4674c28a788b16cbdd314b78
Swapan Mazumdar <swapan.mazumdar@gmail.com>
2020-04-25 00:15:35 +0100

    Solution of Print the Elements of a Linked List

---
+++ src/main/java/PrintTheElementsOfALinkedList_Solution.java
@@ -0,0 +1,56 @@
+/**
+ * Print the Elements of a Linked List
+ */
+public class PrintTheElementsOfALinkedList_Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void printLinkedList(SinglyLinkedListNode head) {
+        while (head != null) {
+            System.out.println(head.data);
+            head = head.next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        for (int i = 0; i < 10; i++) {
+            llist.insertNode(i);
+        }
+
+        printLinkedList(llist.head);
+    }
+}
+

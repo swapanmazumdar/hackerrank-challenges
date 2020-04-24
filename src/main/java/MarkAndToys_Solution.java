71004f38bb6ca273d0d7f9d5c07ad2f7ff566965
Swapan Mazumdar <swapan.mazumdar@gmail.com>
2020-04-24 18:54:33 +0100

    Solution of Mark and Toys

--- src/main/java/MarkAndToys_Solution.java
+++ src/main/java/MarkAndToys_Solution.java
@@ -1,24 +1,30 @@
-import java.io.IOException;
 import java.util.Arrays;
 
 /**
+ * Mark and Toys
+ * <p>
  * Problem:
  * <p>
  * Mark and Jane are very happy after having their first child. Their son loves toys, so Mark wants to buy some. There
  * are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to
  * spend, and he wants to maximize the number of toys he buys with this money.
  * <p>
+ * Given a list of prices and an amount to spend, what is the maximum number of toys Mark can buy? For example, if
+ * prices = [1, 2, 3, 4] and Mark has k = 7 to spend, he can buy items [1, 2, 3] for 6, or [3, 4] for 7 units of
+ * currency. He would choose the first group of 3 items.
+ * <p>
  * Function Description:
  * <p>
- * Complete the function maximumToys in the editor below. It should return an integer representing the maximum number
- * of toys Mark can purchase. maximumToys has the following parameter(s):
- * <p>
- * prices: an array of integers representing toy prices
- * k: an integer, Mark's budget
  */
 public class MarkAndToys_Solution {
 
-    // Complete the maximumToys function below.
+    /**
+     * Complete the function maximumToys in the editor below. It should return an integer representing the maximum number
+     * of toys Mark can purchase. maximumToys has the following parameter(s):
+     * <p>
+     * prices: an array of integers representing toy prices
+     * k: an integer, Mark's budget
+     */
     static int maximumToys(int[] prices, int k) {
         Arrays.sort(prices);
         int cost = 0;
@@ -35,19 +41,8 @@ public class MarkAndToys_Solution {
         return count;
     }
 
-    public static void main(String[] args) throws IOException {
-        int k = 7;
-        int n = 4;
-        int[] prices = new int[n];
-        String[] pricesItems = "1 2 3 4".split(" ");
-
-        for (int i = 0; i < n; i++) {
-            int pricesItem = Integer.parseInt(pricesItems[i]);
-            prices[i] = pricesItem;
-        }
-
-        int result = maximumToys(prices, k);
-        System.out.println(result);
+    public static void main(String[] args) {
+        System.out.println(maximumToys(new int[]{1, 2, 3, 4}, 7));
     }
 }
 

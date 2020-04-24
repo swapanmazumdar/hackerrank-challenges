e4baef54cefde7f365457c5abc1207853d030b9a
Swapan Mazumdar <swapan.mazumdar@gmail.com>
2020-04-24 22:45:16 +0100

    Solution of Sock Merchant

--- src/main/java/SockMerchant_Solution.java
+++ src/main/java/SockMerchant_Solution.java
@@ -1,50 +1,43 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * Sock Merchant
+ * Problem: John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an
+ * array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+ * <p>
+ * For example, there are n = 7 socks with colors arr = [1, 2, 1, 2, 1, 3, 2]. There is one pair of color 1 and one of
+ * color 2. There are three odd socks left, one of each color. The number of pairs is 2.
+ */
 public class SockMerchant_Solution {
 
-    // Complete the sockMerchant function below.
+    /**
+     * Complete the sockMerchant function in the editor below. It must return an integer representing the number of
+     * matching pairs of socks that are available.
+     * <p>
+     * sockMerchant has the following parameter(s):
+     * <p>
+     * n: the number of socks in the pile
+     * ar: the colors of each sock
+     */
     static int sockMerchant(int n, int[] arr) {
         int pairs = 0;
         Set<Integer> colors = new HashSet<>();
-
-        for (int item : arr) {
-            if (!colors.contains(item)) {
-                colors.add(item);
+        for (int i = 0; i < arr.length; i++) {
+            int arrItem = arr[i];
+            if (!colors.contains(arrItem)) {
+                colors.add(arrItem);
             } else {
-                pairs++;
-                colors.remove(item);
+                ++pairs;
+                colors.remove(arrItem);
             }
         }
         return pairs;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] arr = new int[n];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(arrItems[i]);
-        }
-
-        int result = sockMerchant(n, arr);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+    public static void main(String[] args) {
+        System.out.println(sockMerchant(7, new int[]{1, 2, 1, 2, 1, 3, 2}));
+        System.out.println(sockMerchant(9, new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20}));
     }
 }
+

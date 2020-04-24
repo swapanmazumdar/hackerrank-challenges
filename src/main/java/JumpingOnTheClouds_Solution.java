d100e884362f68290dca4ff523e65beff014d380
Swapan Mazumdar <swapan.mazumdar@gmail.com>
2020-04-24 17:28:24 +0100

    Solution of Jumping on the Clouds

--- src/main/java/JumpingOnTheClouds_Solution.java
+++ src/main/java/JumpingOnTheClouds_Solution.java
@@ -1,9 +1,16 @@
-import java.io.IOException;
-import java.util.Scanner;
-
+/**
+ * Emma is playing a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads
+ * and others are cumulus. She can jump on any cumulus cloud having a number that is equal to the number of the current
+ * cloud plus 1 or 2. She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump
+ * from her starting position to the last cloud. It is always possible to win the game.
+ * <p>
+ * For each game, Emma will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided. For example,
+ * c = [0, 1, 0, 0, 0, 1, 0] indexed from 0...6. The number on each cloud is its index in the list so she must avoid the
+ * clouds at indexes 1 and 5. She could follow the following two paths:  0->2->4-6 or 0->2->3->4->6. The first path
+ * takes 3 jumps while the second takes 4.
+ */
 public class JumpingOnTheClouds_Solution {
 
-    // Complete the jumpingOnClouds function below.
     public static int jumpingOnClouds(int[] c) {
         int n = c.length;
         int jumps = -1;
@@ -18,34 +25,9 @@ public class JumpingOnTheClouds_Solution {
         return jumps;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        //int n = scanner.nextInt();
-        int n = 7;
-        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] c = new int[n];
-
-        //String[] cItems = scanner.nextLine().split(" ");
-        String[] cItems = "0 1 0 0 0 1 0".split(" ");
-
-        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int cItem = Integer.parseInt(cItems[i]);
-            c[i] = cItem;
-        }
-
-        int result = jumpingOnClouds(c);
-        System.out.println(result);
-        //bufferedWriter.write(String.valueOf(result));
-        //bufferedWriter.newLine();
-
-        //bufferedWriter.close();
-
-        //scanner.close();
+    public static void main(String[] args) {
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 1, 0, 0, 1, 0}));
+        System.out.println(jumpingOnClouds(new int[]{0, 1, 0, 0, 0, 1, 0}));
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 0, 0, 1, 0}));
     }
 }

6987cdb88542474b1cd0a2cc68a05a2637771b57
Swapan Mazumdar <swapan.mazumdar@gmail.com>
2020-04-27 23:54:43 +0100

    Solve Me First

---
+++ src/main/java/SolveMeFirst_Solution.java
@@ -0,0 +1,10 @@
+public class SolveMeFirst_Solution {
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solveMeFirst(1, 2));
+    }
+}
